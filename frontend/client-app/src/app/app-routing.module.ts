9b5fa6bc83556a4b55080e60a0dfd6f88c61ce81
roizdnaB <danieljambor1999@gmail.com>
2020-12-29 23:21:31 +0100

    Add init routing

--- frontend/client-app/src/app/app-routing.module.ts
+++ frontend/client-app/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
