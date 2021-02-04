2f18e62efa62372e5aa16b73e1fb003c54a1369e
quackeee <krzysztofklak7@gmail.com>
2021-02-04 20:47:16 +0100

    Start implementing courses

--- frontend/client-app/src/app/components/courses/course.service.ts
+++ frontend/client-app/src/app/components/courses/course.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Course } from 'src/app/models/course';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+private courseUrl = environment.apiUrl + 'courses/';
+
+  constructor(private http: HttpClient) { }
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.courseUrl);
+  }
+
+  addCourse(newCourse: Course): Observable<Course> {
+    return this.http.post<Course>(this.courseUrl, newCourse);
+  }
+
+  deleteCourse(course: Course) {
+    return this.http.delete<Course>(this.courseUrl + course._id);
+  }
+
+  editCourse(course: Course) {
+    return this.http.put<Course>(this.courseUrl + course._id, course);
+  }
 }
--- frontend/client-app/src/app/components/courses/courses.component.ts
+++ frontend/client-app/src/app/components/courses/courses.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Course } from 'src/app/models/course';
+import { CourseService } from './course.service';
+import { first } from 'rxjs/operators';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CoursesAddComponent } from './courses-add/courses-add.component';
+import { CoursesEditComponent } from './courses-edit/courses-edit.component';
 
 @Component({
   selector: 'app-courses',
@@ -7,9 +14,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  constructor() { }
+  dataForm: FormGroup;
+  submitted = false;
+  headers = ['title', 'description', 'zoomLink'];
+  courses: Course[];
+
+  constructor(
+    private courseService: CourseService,
+    public dialogAdd: MatDialog,
+    public dialogEdit: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.loadCourses();
+  }
+
+  openDialogAdd(): void {
+    const dialogRef = this.dialogAdd.open(CoursesAddComponent, {
+      width: '250px',
+      height: '400px'
+    });
+
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.loadCourses();
+    });
+  }
+
+  openDialogEdit(course: Course): void {
+    const dialogRef = this.dialogEdit.open(CoursesEditComponent, {
+      width: '250px',
+      height: '400px',
+      data: { eCourse: course }
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.loadCourses();
+    })
+  }
+
+  private loadCourses() {
+    this.courseService.getCourses()
+      .pipe(first())
+      .subscribe(courses => this.courses = courses);
+  }
+
+  onDelete(course: Course): void {
+    this.courseService.deleteCourse(course)
+      .pipe(first())
+      .subscribe(() => {
+        this.loadCourses();
+      });
+  }
+
+  onEdit(): void {
+    // TODO
   }
 
 }
